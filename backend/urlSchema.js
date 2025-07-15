34931e25e806854f8440072ce6e84718d3b78e90
Mohsin Iqbal <mohsiniqbalbhatti0024@gmail.com>
2025-07-15 21:55:16 +0500

    create DB schema for URL

---
+++ backend/urlSchema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const UrlSchema = new mongoose.Schema({
+    url :{
+        type: String,
+        required:true,
+    },
+    shortCode : {
+        type: String,
+        required:true,
+        unique : true
+    },
+    createdAt :{
+        type: Date,
+        default : Date.now()
+    },
+    updatedAt :{
+        type: Date,
+        default : Date.now()
+    }, 
+    accessCount : {
+        type: Number,
+        default: 0
+    }
+})
+
+
+const Url = mongoose.model("Url", UrlSchema) 
+
+export default Url
